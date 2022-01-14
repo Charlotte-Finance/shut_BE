f4900c3891ef141edfbe7110ebda94d269541e01
charlotte <charlotte.finance@etu.univ-lyon1.fr>
2022-01-14 08:56:54 +0100

    🎉 Begin SpringBoot project

---
+++ src/main/java/com/example/shut_be/ShutBeApplication.java
@@ -0,0 +1,39 @@
+package com.example.shut_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@SpringBootApplication
+public class ShutBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShutBeApplication.class, args);
+	}
+
+}
+
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and();
+		http.csrf().disable();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+
+}
